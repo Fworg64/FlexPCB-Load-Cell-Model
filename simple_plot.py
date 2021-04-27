025a85aaa84eca89eeba2ccd3eb45f877db39518
Austin F. Oltmanns <aoltmanns@mines.edu>
2021-04-27 10:51:06 -0600

    updated plot script to pick different values

--- simple_plot.py
+++ simple_plot.py
@@ -5,17 +5,24 @@ import matplotlib.pyplot as plt
 
 names = [];
 data = [];
+time = [];
+
+
+plot_index = 4;
+
 with open('all_exp.txt', newline='') as csvfile:
   myreader = csv.reader(csvfile, delimiter=',')
   for idx,row in enumerate(myreader):
     if idx == 0:
       names = row
     else:
-      data.append(row[2])
+      data.append(row[plot_index])
+      time.append(row[0])
 
 print("Found {0}".format(names))
 
 
 fig = plt.figure()
-plt.plot(data[::50])
+plt.plot(time[:1000:50], data[:1000:50])
+plt.title(names[plot_index])
 plt.show()    
