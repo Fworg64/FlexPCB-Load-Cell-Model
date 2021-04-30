d3fe6b4b28f5c4e61d7855d4636e2f0498af5e04
Austin F. Oltmanns <aoltmanns@mines.edu>
2021-04-30 13:53:03 -0600

    finished outlining methods, stated implementation

--- flexpcbloadcelloptimization/main.py
+++ flexpcbloadcelloptimization/main.py
@@ -10,11 +10,39 @@ def get_ndof(M):   # Given the number of masses for the system
 def get_ndim(M): # Given the number of masses for the system
   return 2*M + 1 # return number of dimensions, ndim, of system (system dynmics matrix is (n x n)
 
-def unpack_x(x):
+def unpack_x(x, M):
 """
 Transform x from (ndof, ) to A matrix (ndim x ndim)
+
+MATLAB Code:
+% Build A
+dof = 2*M*M + 1;
+ndim = 2*M + 1;
+
+syms a [1 dof]
+
+A = [0, 1, zeros(1, (M-1)*2 + 1)];
+A = [A;
+     a(1:ndim)];
+
+for index = 1:(M-1)
+     A = [A;
+          zeros(1,index*2), 0, 1, zeros(1,(M-1-index)*2), 0;
+          a((ndim + 1 + (index-1)*(ndim-1)):(ndim + index*(ndim-1))), 0];
+end
+A = [A; zeros(1,ndim)];
 """
-  pass
+  ndim = get_ndim(M)
+  ndof = get_ndof(M)
+  A = []
+  A.append([0, 1] + [0][0]*((M-1)*2 + 1)
+  A.append([x[index] for index in range(ndim)])
+  for index in range(1,M):
+    A.append([0]*(index*2) + [0, 1] + [0]*((M-1-index)*2)))
+    A.append([x[idx] for idx in ndim + 1 + (index-1)*(ndim-1)):(ndim + index*(ndim-1)))] + [0])
+  A.append([0] * ndim)
+
+  return A
 
 def generate_kalman_objective(params):
 """
@@ -36,23 +64,37 @@ params: dictionary of following terms
   ndim = get_ndim(M)
   ndof = get_ndof(M)
   H = np.zeros(1, M)
-  H[1, p1_idx] = 1.0
-  H[1, p2_idx] = -1.0
+  H[1, 2*p1_idx] = 1.0
+  H[1, 2*p2_idx] = -1.0
   Q = params["Q"]
   R = params["R"]
   zk = params["zk"]
   x0 = params["x0"]
   P0 = params["P0"]
+  F_in = params["F_in"]
 
   def objective_func(x):
-    A = unpack_x(x)
+    A = unpack_x(x, M)
+    state = x0
+    cov = P0
+    meas_err_rec = [0]
+    state_rec    = [state]
+    cov_rec      = [cov] 
+
     for index in range(len(zk)):
-      meas_err, state, cov = calc_Kalman(
+      meas_err, state, cov = calc_Kalman(A, H, Q, R, zk[index], state, cov)
       # record and propagate
-    sum_state_err = 3
-    sum_meas_err  = 3
+      meas_err_rec.append(meas_err)
+      state_rec.append(state)
+      cov_rec.append(cov)
+
+    sum_state_err = np.linalg.norm(state_rec[:,-1] - F_in, 2)
+    sum_meas_err  = np.linalg.norm(meas_err, 2)
+
     return sum_state_err + sum_meas_err
-  pass
+
+  return objective_func
+
 
 def generate_kalman_objective_gradient(params):
   pass
