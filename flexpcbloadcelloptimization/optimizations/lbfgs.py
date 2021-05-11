dfce40f94331afeb9c255b19ed566f2e0728d0de
Austin F. Oltmanns <aoltmanns@mines.edu>
2021-05-10 18:38:26 -0600

    another change

--- flexpcbloadcelloptimization/optimizations/lbfgs.py
+++ flexpcbloadcelloptimization/optimizations/lbfgs.py
@@ -67,7 +67,7 @@ class LBFGS(OptimizationSolver):
       z = Inverse_Hessian_Direction(Yk, Sk, last_grad)
       # line search
       div = 200
-      search_len = 0.0001
+      search_len = 0.000001
       steps = np.zeros((div,1))
       potentialXkfun = np.zeros((div,1))
       for b_index in range(0,div):
